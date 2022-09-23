a6b82c5e1a260141d2bbbe51c40ca289bb6d5cdf
vistca <93658028+vistca@users.noreply.github.com>
2022-09-23 11:10:28 +0200

    säkerhetskopia
    jag ska bara uppdater från master

--- src/main/java/model/graphmanager/algorithms/LinearRegression.java
+++ src/main/java/model/graphmanager/algorithms/LinearRegression.java
@@ -14,7 +14,28 @@ public class LinearRegression implements Algorithm {
 
     @Override
     public DateHashMap<Date, Number> calculate() {
+        DateHashMap<Date, Number> calcData = new DateHashMap<>();
+        double xAxisValue = 0;
+        double sumX = 0;
+        double sumY = 0;
+        double sumXsq = 0;
+        double sumXY = 0;
+        for (Date date : data.keySet()) {
+            xAxisValue++;
+            DayData dayData = data.get(date);
+            double yAxisValue = dayData.getClosed();
+            sumX += xAxisValue;
+            sumY += yAxisValue;
+            sumXsq += Math.pow(xAxisValue, 2);
+            sumXY += xAxisValue * yAxisValue;
 
+        }
+        double denominator = xAxisValue * sumXsq - Math.pow(sumX, 2);
+        double a = (sumY * sumXsq - sumX * sumXY) / denominator;
+        double b = (xAxisValue * sumXY - sumX * sumY) / denominator;
+
+        //calcData.put(date, result);
+        return calcData;
     }
 
 }
