ba10842bd4f74ddb0fd740eae868bc4199a87221
Liten Chungus <112545226+LitenChungus@users.noreply.github.com>
2022-09-25 16:38:26 +0200

    Controller Functionality
    Degrees of precision to increase preformance

--- src/main/java/controller/AppController.java
+++ src/main/java/controller/AppController.java
@@ -1,9 +1,13 @@
 package controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import model.AppModel;
@@ -18,19 +22,61 @@ import java.net.URL;
 import java.util.*;
 
 public class AppController implements Initializable {
-    private final AppModel model = AppModel.getInstance();
-    private ArrayList<String> activeCompanies;
-    @FXML
-    FlowPane stockPane;
-    private Map<String, ControllerStockListItem> stockListItemMap = new HashMap<String, ControllerStockListItem>();
-    @FXML private LineChart<String, Number> chart1;
-    @Override
 
+    private final AppModel model = AppModel.getInstance();
+    private Map<String, ControllerStockListItem> stockListItemMap = new HashMap<String, ControllerStockListItem>();
+    private ArrayList<String> activeCompanies;
+    private int precisionAmount;
+
+    @FXML
+    private FlowPane stockPane;
+
+    @FXML
+    private LineChart<String, Number> chart1;
+
+    @FXML
+    private Spinner<Integer> precisionSpinner;
+
+    @FXML
+    public Spinner<Integer> yearSpinner;
+
+    @FXML
+    public Spinner<Integer> monthSpinner;
+
+    @FXML
+    public Spinner<Integer> daySpinner;
+
+    @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        activeCompanies = new ArrayList<String>();
+        initializeVariables();
         initializeStockPane();
+        initializeSpinners();
         updateStockList();
     }
+
+    private void initializeVariables() {
+        activeCompanies = new ArrayList<String>();
+        precisionAmount = 1;
+    }
+
+    private void initializeSpinners() {
+        SpinnerValueFactory<Integer> precisionValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 10, 1);
+        precisionSpinner.setValueFactory(precisionValueFactory);
+
+        precisionSpinner.valueProperty().addListener((observableValue, oldValue, newValue) -> {
+            precisionAmount = newValue;
+            updateStockList();
+        });
+
+        precisionSpinner.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                precisionAmount = Integer.parseInt(precisionSpinner.getEditor().getText());
+                refreshStocks();
+            }
+        });
+    }
+
+
 
     private void initializeStockPane() {
         stockPane.getChildren().clear();
@@ -58,19 +104,19 @@ public class AppController implements Initializable {
         series1.setName(acronym);
         try {
             Date date = new Date(2012,1,1);
-            Object close = null;
-            for(int i = 0; i<data.size(); i++) {
-                boolean go;
-                do {
-                    go = true;
-                    date = date.nextDate();
-                    try {
-                        close = data.get(date).getClosed();
-                    } catch (NullPointerException e) {
-                        go = false;
-                    }
-                } while (!go);
-                series1.getData().add(i, new XYChart.Data<>(date.toString(), (double)close));
+            double valueToAdd = 0;
+            for(int i = 0, currentCount = 1, slot = 0; i<data.size();) {
+                if (data.get(date) != null) {
+                    valueToAdd += data.get(date).getClosed();
+                    if (currentCount == precisionAmount) {
+                        valueToAdd = valueToAdd/precisionAmount;
+                        series1.getData().add(slot, new XYChart.Data<>(date.toString(), valueToAdd));
+                        valueToAdd = 0;
+                        currentCount = 1;
+                        slot++;
+                    } else currentCount++;
+                    i++;
+                } date = date.nextDate();
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -79,11 +125,18 @@ public class AppController implements Initializable {
         chart1.getData().add(series1);
     }
 
+    private void refreshStocks() {
+        for (String activeCompany : activeCompanies) {
+            chart1.getData().remove(0);
+            addStockToGraph(activeCompany);
+        }
+    }
+
     private boolean checkActiveCompanies(String acronym) {
         if (activeCompanies.contains(acronym)) {
-            int index = activeCompanies.indexOf(acronym);
-            chart1.getData().remove(index);
-            activeCompanies.remove(index);
+            int i = activeCompanies.indexOf(acronym);
+            chart1.getData().remove(i);
+            activeCompanies.remove(i);
             return true;
         }
         else return false;
