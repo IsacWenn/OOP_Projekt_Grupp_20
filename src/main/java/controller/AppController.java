b89c18064cd6ab28aad66cc92e5786367d3ec6b1
vistca <93658028+vistca@users.noreply.github.com>
2022-09-29 12:30:58 +0200

    big problem

--- src/main/java/controller/AppController.java
+++ src/main/java/controller/AppController.java
@@ -10,9 +10,7 @@ import model.datahandling.DataHandler;
 import model.datahandling.DateHashMap;
 import model.datahandling.DayData;
 import model.graphmodel.GraphModel;
-import model.graphmodel.graphablefunctions.DailyChange;
-import model.graphmodel.graphablefunctions.DailyHighMinusLow;
-import model.graphmodel.graphablefunctions.LinearRegression;
+
 
 import java.io.IOException;
 import java.net.URL;
@@ -30,23 +28,21 @@ public class AppController implements Initializable {
         XYChart.Series<String, Number> series2 = new XYChart.Series<>();
         series1.setName("Stock");
         series2.setName("LinReg");
-        populateSeries(series1, "AAPL", false);
-        populateSeries(series2, "AAPL", true);
+        populateSeries(series1, "AAPL");
 
         chart1.setCreateSymbols(false);
         //chart1.getData().add(series1);
         chart1.getData().add(series2);
     }
 
-    private void populateSeries(XYChart.Series<String, Number> series, String mic, Boolean lin) {
+    private void populateSeries(XYChart.Series<String, Number> series, String mic) {
         DateHashMap<Date, DayData> data = DataHandler.getCompanyData(mic);
         try {
             Date date1 = new Date(2020,1,1);
-            Date date2 = new Date(2021,2,1);
+            Date date2 = new Date(2020,3,1);
 
             GraphModel graphModel = new GraphModel("TSLA", date1, date2);
-            if (lin)
-                graphModel.updateAlgorithm(new DailyHighMinusLow());
+            graphModel.changeCurrency("SEK_USD.csv");
             graphModel.update();
 
             DateHashMap<Date, Number> values = graphModel.getValues();
