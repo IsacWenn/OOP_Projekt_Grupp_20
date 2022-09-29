f6ac318157710a07e095c3e281179cc1fde0efdc
IsacWenn <s01isin79@gmail.com>
2022-09-29 10:34:35 +0200

    Create CurrencyEnum.java

---
+++ src/main/java/model/util/CurrencyEnum.java
@@ -0,0 +1,11 @@
+package model.util;
+
+/**
+ * An Enum representing the different types of currency's available in the model.
+ */
+public enum CurrencyEnum {
+    USD, SEK, EUR,
+    GBP, RUB, JPY,
+    INR, KRW, NOK,
+    DKK, ISK
+}
