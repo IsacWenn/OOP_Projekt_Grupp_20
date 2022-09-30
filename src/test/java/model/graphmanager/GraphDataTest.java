f88d0042f7f9d0623ef8612cf7ed76c02ca30843
Pontanami <pontusen@student.chalmers.se>
2022-09-30 18:18:19 +0200

    Tests for Graph and GraphData

--- src/test/java/model/graphmanager/GraphDataTest.java
+++ src/test/java/model/graphmanager/GraphDataTest.java
@@ -1,15 +1,64 @@
 package model.graphmanager;
 
 
-import org.junit.jupiter.api.BeforeEach;
+import model.datahandling.DayData;
+import model.util.Date;
+import org.junit.jupiter.api.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GraphDataTest {
-    private GraphData graphData;
+
+    private static Date date1;
+    private static Date date2;
+    private static Date date3;
+
+    private static List<Date> dateList;
+
+    static GraphData graphData;
 
     @BeforeEach
-    public void create_Instance(){
-        GraphData graphData = new GraphData();
+    public void create_Variables() throws IOException {
+        graphData = new GraphData();
+
+        date1 = new Date(2022, 6, 21);
+        date2 = new Date(2022, 7, 20);
+        date3 = new Date(2022, 7, 21);
+
+        dateList = new ArrayList<>(){{
+            add(date1);
+            add(date2);
+            add(date3);
+        }};
+
     }
 
+    @Test
+    public void getCompanyDataWithOnlyMicShouldReturnAllAvailableDataForThatCompany(){
+        Map<Date, DayData> data = graphData.getCompanyData("AAPL");
+        assertEquals(2515, data.size());
+        assertNotNull(data.get(date1));
+    }
+
+    @Test
+    public void getCompanyDataWithMicAndIntervalShouldReturnDataForThatInterval(){
+        Map<Date, DayData> data = graphData.getCompanyData("AAPL", date1, date2);
+        assertEquals(21, data.size());
+    }
+
+    @Test
+    public void getCompanyDataWithMicAndListOfDatesShouldReturnDataForThoseDates(){
+        Map<Date, DayData> data = graphData.getCompanyData("AAPL", dateList);
+        assertEquals(3, data.size());
+    }
+
+    @Test
+    public void getCurrencyDataCalledWithACurrencyShouldReturnTheExchangeForThatCurrency(){
+
+    }
 }
--- src/test/java/model/graphmanager/GraphTest.java
+++ src/test/java/model/graphmanager/GraphTest.java
@@ -1,52 +1,79 @@
 package model.graphmanager;
 
+import model.graphmanager.algorithms.Graphables;
 import model.util.Date;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GraphTest {
+
+    private static Date date1;
+    private static Date date2;
+    private static Date date3;
+
+    private static List<Date> dateList;
+
+
+    @BeforeEach
+    public void createTestVariables() throws IOException {
+        date1 = new Date(2022, 6, 21);
+        date2 = new Date(2022, 7, 20);
+        date3 = new Date(2022, 7, 21);
+
+        dateList = new ArrayList<>(){{
+            add(date1);
+            add(date2);
+            add(date3);
+        }};
+    }
 
     @Test
     public void graphModelCreatedWithOnlyMicShouldContainAllAvailableCompanyData() {
         Graph graph = new Graph("AAPL");
         assertEquals(2515, graph.data.size());
+        assertNotNull(graph.data.get(date1));
     }
 
     @Test
     public void graphModelCreatedWithMicAndDateIntervalShouldContainDataForThatInterval() {
-        try {
-            Date date1 = new Date(2022, 6, 20);
-            Date date2 = new Date(2022,7, 21);
-            Graph graph = new Graph("AAPL", date1, date2);
-            assertEquals(22, graph.data.size());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Graph graph = new Graph("AAPL", date1, date2);
+        assertEquals(21, graph.data.size());
     }
     @Test
     public void graphModelCreatedWithAListOfDatesShouldContainDataForThoseDates(){
-        try{
-            Date date = new Date(2022, 6, 20);
-            Date dateTo = new Date(2022, 7, 20);
-            List<Date> dates = date.listIntervalTo(dateTo);
-            Graph graph = new Graph("AAPL", dates);
-            assertEquals(21, graph.data.size());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Graph graph = new Graph("AAPL", dateList);
+        assertEquals(3, graph.data.size());
     }
-    @Test void updateMethodInGraphModelShouldUpdateTheValuesOfTheGraphModel(){
-        try{
-            Date date1 = new Date(2022, 6, 20);
-            Date date2 = new Date(2022, 7, 20);
-            Graph graph = new Graph("AAPL", date1, date2);
+    @Test
+    public void updateMethodShouldUpdateTheValuesOfTheGraphModel(){
+        Graph graph = new Graph("AAPL", date1, date2);
+        graph.update();
+        assertEquals(135.87, graph.getValues().get(date1));
+    }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @Test
+    public void updateAlgorithmShouldUpdateTheCurrentAlgorithm(){
+        Graph graph = new Graph("AAPL", date2, date3);
+        graph.updateAlgorithm(Graphables.DAILYCHANGE);
+        graph.update();
+        assertEquals(1.27, (double)graph.getValues().get(date2), 0.01);
+    }
 
+    @Test
+    public void changeCurrencyShouldUpdateWhatCurrencyTheGraphValuesIsShownIn(){
+        Graph graph = new Graph("AAPL", date2, date3);
+        graph.changeCurrency("SEK_USD.csv");
+        graph.update();
+        //assertEquals(1569.95, (double)graph.getValues().get(date2), 0.1); TODO
+    }
+
+    @Test
+    public void getValuesShouldReturnTheCurrentlyCalculatedValuesInGraphModel(){
+        Graph graph = new Graph("AAPL", date2, date3);
+        //TODO
     }
 }
