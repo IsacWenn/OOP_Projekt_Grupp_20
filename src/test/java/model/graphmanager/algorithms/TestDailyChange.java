8cd7ae5cea1ae109169295120af1b9b0fd3096a1
vistca <93658028+vistca@users.noreply.github.com>
2022-09-30 17:42:46 +0200

    test for dailyChange

---
+++ src/test/java/model/graphmanager/algorithms/TestDailyChange.java
@@ -0,0 +1,41 @@
+package model.graphmanager.algorithms;
+
+import model.datahandling.DateHashMap;
+import model.datahandling.DayData;
+import model.util.Date;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+public class TestDailyChange {
+
+    private static DateHashMap<Date, DayData> data;
+    private static DateHashMap<Date, Number> calculatedData;
+    private static DayData dayData1;
+    private static DayData dayData2;
+    private static Date date1;
+    private static Date date2;
+    private static DailyChange dailyChange;
+
+    @BeforeEach
+    public void setVariables() throws IOException {
+        data = new DateHashMap<Date, DayData>();
+        dayData1 = new DayData(100,100,200,1000,50);
+        dayData2 = new DayData(8,4,2,10,1);
+        date1 = new Date(2020, 1, 1);
+        date2 = new Date(2020, 1, 2);
+        data.put(date1,dayData1);
+        data.put(date2,dayData2);
+        dailyChange = new DailyChange();
+        calculatedData = dailyChange.calculate(data);
+    }
+
+    @Test
+    public void returnDataShouldBeExpressedInCorrectPercentages() {
+        assertEquals(100.0, calculatedData.get(date1));
+        assertEquals(-50.0,calculatedData.get(date2));
+    }
+
+}
