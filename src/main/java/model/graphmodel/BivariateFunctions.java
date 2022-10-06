683fee6a80ca4cbb810fe124130833326869b995
vistca <93658028+vistca@users.noreply.github.com>
2022-10-06 14:56:35 +0200

    correlation class has been created

---
+++ src/main/java/model/graphmodel/BivariateFunctions.java
@@ -0,0 +1,9 @@
+package model.graphmodel;
+
+import model.util.Date;
+
+import java.util.Map;
+
+public interface BivariateFunctions {
+    double calculateKeyFigure(Map<Date, Number> series1, Map<Date, Number> series2);
+}
---
+++ src/main/java/model/graphmodel/PearsonCorrelation.java
@@ -0,0 +1,32 @@
+package model.graphmodel;
+
+import model.graphmodel.BivariateFunctions;
+import model.util.Date;
+
+import java.util.Map;
+
+public class PearsonCorrelation implements BivariateFunctions {
+
+    @Override
+    public double calculateKeyFigure(Map<Date, Number> series1, Map<Date, Number> series2) {
+        double sumX = 0;
+        double sumY = 0;
+        double sumXY = 0;
+        double sumXX = 0;
+        double sumYY = 0;
+        int n = series1.size();
+
+        for (Date date : series1.keySet()) {
+            double x = (double) series1.get(date);
+            double y = (double) series2.get(date);
+            sumX += x;
+            sumY += y;
+            sumXY += x * y;
+            sumXX += x * x;
+            sumYY += y * y;
+        }
+
+        return (n * sumXY - sumX * sumY) /
+                Math.sqrt((n * sumXX - Math.pow(sumX, 2)) * (n * sumYY - Math.pow(sumY, 2)) );
+    }
+}
