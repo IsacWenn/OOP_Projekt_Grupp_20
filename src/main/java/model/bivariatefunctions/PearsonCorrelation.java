1b2b6e8d6dc571789fc558bdfe9899df4a181084
vistca <93658028+vistca@users.noreply.github.com>
2022-10-07 16:13:47 +0200

    java docs and intersection
    Additional javaDocs and the PearsonCorrelation-class now does only calculate the correlation based on the dates the two data sets have in common

--- src/main/java/model/bivariatefunctions/PearsonCorrelation.java
+++ src/main/java/model/bivariatefunctions/PearsonCorrelation.java
@@ -3,10 +3,23 @@ package model.bivariatefunctions;
 import model.bivariatefunctions.BivariateFunctions;
 import model.util.Date;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
+/**
+ * A class used for calculating the Pearson correlation.
+ */
 
 public class PearsonCorrelation implements BivariateFunctions {
 
+    /**
+     * A method for calculating the Pearson correlation coefficient between two data sets.
+     * @param series1 a {@link Map} containing a {@link Date} and a {@link Number} representing one data set.
+     * @param series2 a {@link Map} containing a {@link Date} and a {@link Number} representing one data set.
+     * @return a {@link Double} that is the calculated correlation coefficient.
+     */
     @Override
     public double calculateKeyFigure(Map<Date, Number> series1, Map<Date, Number> series2) {
         double sumX = 0;
@@ -15,6 +28,8 @@ public class PearsonCorrelation implements BivariateFunctions {
         double sumXX = 0;
         double sumYY = 0;
         int n = series1.size();
+
+        series1.keySet().retainAll(series2.keySet());
 
         for (Date date : series1.keySet()) {
             double x = (double) series1.get(date);
@@ -29,4 +44,6 @@ public class PearsonCorrelation implements BivariateFunctions {
         return (n * sumXY - sumX * sumY) /
                 Math.sqrt((n * sumXX - Math.pow(sumX, 2)) * (n * sumYY - Math.pow(sumY, 2)) );
     }
+
 }
+
