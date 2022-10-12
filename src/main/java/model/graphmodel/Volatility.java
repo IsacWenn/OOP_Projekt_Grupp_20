500b9826e7c73f71bbdabd2f16c27108b8960e42
Pontanami <pontusen@student.chalmers.se>
2022-10-12 17:26:33 +0200

    Start of KeyFigure implementation

---
+++ src/main/java/model/graphmodel/Volatility.java
@@ -0,0 +1,25 @@
+package model.graphmodel;
+
+import model.datahandling.DayData;
+import model.util.Date;
+
+import java.util.Map;
+
+public class Volatility implements KeyFigureAlgorithm{
+
+    @Override
+    public Double calculate(Map<Date, DayData> data) {
+        double mean = 0;
+        double standardDeviation = 0;
+        for(Date date : data.keySet()){
+            DayData dayData = data.get(date);
+            mean+= dayData.getClosed()/data.size();
+        }
+
+        for(Date date : data.keySet()){
+            DayData dayData = data.get(date);
+            standardDeviation += Math.pow(dayData.getClosed()-mean, 2);
+        }
+        return Math.sqrt(standardDeviation/data.size());
+    }
+}
