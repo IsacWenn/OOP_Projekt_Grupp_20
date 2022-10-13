633edb0977092a6ba321061a2aba47501ae159d1
vistca <93658028+vistca@users.noreply.github.com>
2022-10-13 10:12:07 +0200

    four new key figure algorithms

---
+++ src/main/java/model/graphmodel/AveragePrice.java
@@ -0,0 +1,30 @@
+package model.graphmodel;
+
+import model.datahandling.DayData;
+import model.graphmodel.KeyFigureAlgorithm;
+import model.util.Date;
+
+import java.util.Map;
+
+/**
+ * A class implementing {@link KeyFigureAlgorithm} for computing the average price of an asset traded during the period of the provided data.
+ */
+public class AveragePrice implements KeyFigureAlgorithm {
+
+    /**
+     * A method doing the calculation for the average price of a stock.
+     * @param data is a {@link Map} containing information about the asset as a {@link DayData} for each {@link Date}.
+     * @return a {@link Double} representing the average price of the asset.
+     */
+    @Override
+    public Double calculate(Map<Date, DayData> data) {
+        double sumOfVolumeInNumberOfStocks = 0;
+        double sumOfVolumeInMoney = 0;
+        for (Date date : data.keySet()) {
+            DayData dayData = data.get(date);
+            sumOfVolumeInNumberOfStocks += dayData.getVolume();
+            sumOfVolumeInMoney += dayData.getVolume() * dayData.getClosed();
+        }
+        return sumOfVolumeInMoney / sumOfVolumeInNumberOfStocks;
+    }
+}
---
+++ src/main/java/model/graphmodel/AverageVolume.java
@@ -0,0 +1,25 @@
+package model.graphmodel;
+
+import model.datahandling.DayData;
+import model.util.Date;
+
+import java.util.Map;
+
+/**
+ * A class implementing {@link KeyFigureAlgorithm} for computing the average trading volume for an asset.
+ */
+public class AverageVolume implements KeyFigureAlgorithm {
+
+    /**
+     * A method doing the calculation for the average trading volume.
+     * @param data is a {@link Map} containing information about the asset as a {@link DayData} for each {@link Date}.
+     * @return a {@link Double} representing the average trading volume.
+     */
+    @Override
+    public Double calculate(Map<Date, DayData> data) {
+        double sumOfVolume = 0;
+        for (Date date: data.keySet())
+            sumOfVolume += data.get(date).getVolume();
+        return sumOfVolume / data.size();
+    }
+}
