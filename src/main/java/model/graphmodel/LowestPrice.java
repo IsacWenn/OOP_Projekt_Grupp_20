fb11fdfee6e452af14c3fad36089e422388088ee
vistca <93658028+vistca@users.noreply.github.com>
2022-10-13 10:12:07 +0200

    four new key figure algorithms

---
+++ src/main/java/model/graphmodel/LowestPrice.java
@@ -0,0 +1,28 @@
+package model.graphmodel;
+
+import model.datahandling.DayData;
+import model.util.Date;
+
+import java.util.Map;
+
+/**
+ * A class implementing {@link KeyFigureAlgorithm} for finding the lowest price of an asset.
+ */
+public class LowestPrice implements KeyFigureAlgorithm {
+
+    /**
+     * A method that finds the lowest price during.
+     * @param data is a {@link Map} containing information about the asset as a {@link DayData} for each {@link Date}.
+     * @return a {@link Double} representing the lowest price of the asset.
+     */
+    @Override
+    public Double calculate(Map<Date, DayData> data) {
+        Double lowestPrice = null;
+        for (Date date : data.keySet()) {
+            double priceOfTheDay = data.get(date).getClosed();
+            if ((lowestPrice == null) || (priceOfTheDay < lowestPrice))
+                lowestPrice = priceOfTheDay;
+        }
+        return lowestPrice;
+    }
+}
