252763ca6376f27638d35b28b115d693f95ff9cd
vistca <93658028+vistca@users.noreply.github.com>
2022-10-14 12:55:52 +0200

    graphalgorithm package now has javaDocs and tests

--- src/main/java/model/graphmodel/graphalgorithms/GraphAlgorithmCollection.java
+++ src/main/java/model/graphmodel/graphalgorithms/GraphAlgorithmCollection.java
@@ -1,16 +1,27 @@
 package model.graphmodel.graphalgorithms;
 
+
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * A class used for retrieving different {@link GraphAlgorithm}.
+ */
 public class GraphAlgorithmCollection {
 
+    /**
+     * A static {@link Map} used to store a collection of {@link GraphAlgorithm}.
+     */
     private static Map<String, GraphAlgorithm> algorithms = null;
 
-    private GraphAlgorithmCollection() {
-    }
 
+    /**
+     * A method for creating the implementations of {@link GraphAlgorithm} if they are not already created and
+     *
+     * put them in the static variable {@link GraphAlgorithmCollection#algorithms}.
+     */
     public static void init() {
         if (algorithms == null) {
             algorithms = new HashMap<>();
@@ -21,9 +32,20 @@ public class GraphAlgorithmCollection {
         }
     }
 
+    /**
+     * A method for retrieving a {@link GraphAlgorithm} created from {@link GraphAlgorithmCollection#algorithms}.
+     *
+     * @param graphAlg is a {@link String} used to get a specific implementation of from {@link GraphAlgorithmCollection#algorithms}.
+     * @return A {@link GraphAlgorithm}.
+     */
     public static GraphAlgorithm getGraphAlgorithm(String graphAlg) {
         return algorithms.get(graphAlg);
     }
 
+    /**
+     * A method for retrieving the {@link String} key set for all {@link GraphAlgorithm} in {@link GraphAlgorithmCollection#algorithms}.
+     *
+     * @return A {@link Set} of {@link String} for each {@link GraphAlgorithm} in {@link GraphAlgorithmCollection#algorithms}.
+     */
     public static Set<String> getKeySet(){return algorithms.keySet();}
 }
--- src/test/java/model/bivariatealgorithms/TestBivariateAlgorithmCollection.java
+++ src/test/java/model/bivariatealgorithms/TestBivariateAlgorithmCollection.java
@@ -1,16 +1,28 @@
 package model.bivariatealgorithms;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestBivariateAlgorithmCollection {
 
+    @BeforeAll
+    static void initBivariateAlgorithmsCollection() {
+        BivariateAlgorithmCollection.init();
+    }
+
     @Test
     public void classForCalculatingPearsonCorrelationShouldBeRetrieved() {
-        BivariateAlgorithmCollection.init();
         BivariateAlgorithms ba  = BivariateAlgorithmCollection.getGraphAlgorithms("Pearson correlation");
         assertTrue(ba instanceof PearsonCorrelation);
     }
+
+    @Test
+    public void methodShouldRetrieveASpecificAlgorithmName() {
+        int numberOfAlgorithms = BivariateAlgorithmCollection.getKeySet().size();
+        assertNotEquals(0,numberOfAlgorithms);
+    }
+
 
 }
