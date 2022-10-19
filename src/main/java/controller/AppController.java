c45203b5e3253fcd5ed8be19b33980aac04a4dde
Liten Chungus <112545226+LitenChungus@users.noreply.github.com>
2022-10-19 10:47:17 +0200

    Added documentation to AppController and removed empty methods.

--- src/main/java/controller/AppController.java
+++ src/main/java/controller/AppController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -10,36 +9,42 @@ import model.AppModel;
 import java.net.URL;
 import java.util.*;
 
-public class AppController implements Initializable {
+public class AppController {
 
     private final AppModel appModel = AppModel.getInstance();
 
     @FXML
     private TabPane tabsPane;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        initializeVariables();
-    }
-
-    private void initializeVariables() {
-    }
-
+    /**
+     * Generates a new {@link ComparisonChartController} as a tab in the AppController GUI.
+     */
     public void newComparisonChart() {
         ChartController newChart = new ComparisonChartController(this);
         newTab(newChart, "Comparison Chart");
     }
 
+    /**
+     * Generates a new {@link DetailedChartController} as a tab in the AppController GUI.
+     */
     public void newDetailedChart() {
         ChartController newChart = new DetailedChartController(this);
         newTab(newChart, "Detailed Chart");
     }
 
+    /**
+     * Generates a new {@link CorrelationChartController} as a tab in the AppController GUI.
+     */
     public void newCorrelationChart() {
         ChartController newChart = new CorrelationChartController(this);
         newTab(newChart, "Correlation Chart");
     }
 
+    /**
+     * Creates a new tab in the AppController GUI.
+     * @param content the node which is added as a tab.
+     * @param name the name of the tab.
+     */
     private void newTab(Node content, String name) {
         Tab newTab = new Tab(name, content);
         tabsPane.getTabs().add(newTab);
