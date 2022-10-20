545268f13efe53fc58c11207a6614c2886e89fe3
Liten Chungus <112545226+LitenChungus@users.noreply.github.com>
2022-10-20 15:53:06 +0200

    todo

--- src/main/java/controller/ControllerStockListItem.java
+++ src/main/java/controller/ControllerStockListItem.java
@@ -66,6 +66,7 @@ public class ControllerStockListItem extends AnchorPane {
     private void initializeLabels(String acronym) {
         stockAcronym.setText(acronym);
         stockName.setText(name);
+        //TODO stockValue.settext(VALUEEEE);
     }
 
     /**
