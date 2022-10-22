6a36b90e019d553e583aa724b6e7ac3769a6cdf3
IsacWenn <s01isin79@gmail.com>
2022-10-22 13:58:46 +0200

    more cleanup

--- src/main/java/model/AppModel.java
+++ src/main/java/model/AppModel.java
@@ -1,23 +1,24 @@
 package model;
 
-import model.bivariatealgorithms.BivariateAlgorithmCollection;
 import model.datahandling.DataHandler;
-import model.graphmodel.graphalgorithms.GraphAlgorithmCollection;
-import model.graphmodel.keyfigures.KeyFigureCollection;
 import view.Observable;
 
 import java.util.ArrayList;
 
 public class AppModel {
 
+    /**
+     * A list of {@link Observable}s that can be notified.
+     */
     private ArrayList<Observable> observerList;
+    /**
+     * Static variable holding the instance of {@link AppModel}.
+     */
     private static AppModel instance = null;
-    private DataHandler dataHandler;
 
-    public static void main(String[] args) {
-
-    }
-
+    /**
+     * A constructor for the class AppModel.
+     */
     private AppModel() {
         observerList = new ArrayList<>();
     }
